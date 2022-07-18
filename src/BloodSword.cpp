9d933b35a98e6d7c60cf038e0eb648b49ad585af
Dael <dael.separa@gmail.com>
2022-07-18 23:32:21 +0800

    Initial Skeleton

---
+++ src/BloodSword.cpp
@@ -0,0 +1,14 @@
+#include "BloodSword.hpp"
+
+namespace BloodSword
+{
+    int Main(int argc, char **argv)
+    {
+        return 0;
+    }
+}
+
+int main(int argc, char **argv)
+{
+    return BloodSword::Main(argc, argv);
+}
---
+++ src/BloodSword.hpp
@@ -0,0 +1,11 @@
+#ifndef __BLOODSWORD_HPP__
+#define __BLOODSWORD_HPP__
+
+#include "Engine.hpp"
+
+namespace BloodSword
+{
+
+}
+
+#endif
