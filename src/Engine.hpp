d4266797baa56ea96383c6abe6ba914531e419db
Dael <dael.separa@gmail.com>
2022-07-22 07:51:23 +0800

    Add random number generator

--- src/Engine.hpp
+++ src/Engine.hpp
@@ -3,10 +3,17 @@
 
 #include "Graphics.hpp"
 #include "Character.hpp"
+#include "Random.hpp"
 
 namespace BloodSword::Engine
 {
+    auto Random = Random::Base();
 
+    void Randomize()
+    {
+        BloodSword::Engine::Random.UniformIntDistribution(1, 6);
+        BloodSword::Engine::Random.UniformDistribution(0, 1.0);
+    }
 }
 
 #endif
