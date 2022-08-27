3fce4e635f6b29ac61a705acb7c3fc63daa35f51
Dael <dael.separa@gmail.com>
2022-08-27 09:21:16 +0800

    fix unused LoadError warning

--- src/Map.hpp
+++ src/Map.hpp
@@ -150,7 +150,7 @@ namespace BloodSword::Map
 
                 this->Height = !data["height"].is_null() ? (int)data["height"] : 0;
 
-                auto LoadError = false;
+                LoadError = false;
 
                 if (this->Width > 0 && this->Height > 0)
                 {
