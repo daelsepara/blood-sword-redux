d3b9cabe5cd2d1c2d74a7952a5412741f63d69e5
Dael <dael.separa@gmail.com>
2022-11-21 23:54:55 +0800

    Update Conditions.hpp

--- src/Conditions.hpp
+++ src/Conditions.hpp
@@ -1,6 +1,7 @@
 #ifndef __CONDITIONS_HPP__
 #define __CONDITIONS_HPP__
 
+#include "nlohmann/json.hpp"
 #include "Book.hpp"
 #include "Templates.hpp"
 
@@ -9,6 +10,7 @@ namespace BloodSword::Conditions
     enum class Type
     {
         NONE = -1,
+        NORMAL,
         CHARACTER,
         ITEM,
         BATTLE,
@@ -49,5 +51,63 @@ namespace BloodSword::Conditions
         {
         }
     };
+
+    Conditions::Base Parse(nlohmann::json json)
+    {
+        auto condition = Conditions::Base();
+
+        if (!json["condition"].is_null())
+        {
+            auto data = json["condition"];
+
+            if (!data["destination"].is_null())
+            {
+                // set destination
+                auto book = !data["destination"]["book"].is_null() ? Book::MapBook(std::string(data["destination"]["book"])) : Book::Number::NONE;
+
+                auto number = !data["destination"]["number"].is_null() ? std::stoi(std::string(data["destination"]["number"])) : 0;
+
+                condition.Destination = {book, number};
+            }
+
+            if (!data["type"].is_null())
+            {
+                // set choice type
+                condition.Type = Conditions::Map(std::string(data["type"]));
+            }
+
+            if (!data["text"].is_null())
+            {
+                // set attribute
+                condition.Text = std::string(data["text"]);
+            }
+
+            if (!data["variable"].is_null())
+            {
+                // set character class
+                condition.Variable = std::string(data["variable"]);
+            }
+        }
+
+        return condition;
+    }
+
+    bool Process(Conditions::Base condition)
+    {
+        auto result = false;
+
+        switch (condition.Type)
+        {
+        case Conditions::Type::NONE:
+            result = true;
+        case Conditions::Type::NORMAL:
+            result = true;
+            break;
+        default:
+            result = true;
+        }
+
+        return result;
+    }
 }
 #endif
