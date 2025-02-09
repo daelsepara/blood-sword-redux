8e09414b945b8c81edf34f9bfb26341cf379b829
Dael <dael.separa@gmail.com>
2025-02-09 20:09:48 +0800

    add enthralment victory condition in battle test

--- src/Test.hpp
+++ src/Test.hpp
@@ -1814,6 +1814,9 @@ namespace BloodSword::Test
         case Battle::Result::DEFEAT:
             Interface::MessageBox(graphics, scene, Graphics::RichText("YOUR PARTY HAS BEEN DEFEATED!", Fonts::Normal, Color::Active, TTF_STYLE_NORMAL, 0), Color::Transparent, Color::Highlight, 4, Color::Highlight, true);
             break;
+        case Battle::Result::ENTHRALLED:
+        Interface::MessageBox(graphics, scene, Graphics::RichText("ENEMIES HAVE BEEN SUBDUED!", Fonts::Normal, Color::Active, TTF_STYLE_NORMAL, 0), Color::Transparent, Color::Active, 4, Color::Highlight, true);
+            break;
         case Battle::Result::FLEE:
             Interface::MessageBox(graphics, scene, Graphics::RichText("YOUR PARTY FLEES!", Fonts::Normal, Color::Active, TTF_STYLE_NORMAL, 0), Color::Transparent, Color::Active, 4, Color::Highlight, true);
             break;
