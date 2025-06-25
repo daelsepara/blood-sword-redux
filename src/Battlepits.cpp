aa5e21d2c4b4c28c9ef28a39183a39bc63340410
Dael <dael.separa@gmail.com>
2025-06-26 01:19:35 +0800

    progress battlepits generator

---
+++ src/Battlepits.cpp
@@ -0,0 +1,166 @@
+#include "Room.hpp"
+#include "Interface.hpp"
+
+// render map to a png file
+namespace BloodSword::BattlepitsRenderer
+{
+    void RenderPoints(SDL_Surface *surface, Points &points, SDL_Rect &rect, int offset, Asset::Type asset, bool blur = false)
+    {
+        for (auto point : points)
+        {
+            rect.x = point.X * BloodSword::TileSize + offset;
+
+            rect.y = point.Y * BloodSword::TileSize + offset;
+
+            auto surface_sset = blur ? BloodSword::Asset::Surface(asset, 0x7F7F7F7F) : BloodSword::Asset::Surface(asset);
+
+            Graphics::RenderAssetThenFree(surface, surface_sset, rect);
+        }
+    }
+
+    void Render(const char *module, const char *image_file)
+    {
+        // get all available modules
+        Interface::LoadModules();
+
+        // load selected module
+        Interface::LoadModule(module);
+
+        // load settings
+        Interface::Initialize(Interface::SettingsFile.c_str());
+
+        // load asset location mappings
+        Asset::Load(std::string(Interface::Settings["assets"]).c_str());
+
+        // load specific map
+        auto map = Map::Base(65, 45);
+
+        // generate battlepits
+        Battlepits::Generate(map);
+
+        SDL_Rect rect;
+
+        auto offset = BloodSword::LargePad;
+
+        rect.w = map.Width * BloodSword::TileSize + offset * 2;
+
+        rect.h = map.Height * BloodSword::TileSize + offset * 2;
+
+        auto surface = Graphics::CreateSurface(rect.w, rect.h);
+
+        if (surface)
+        {
+            // fill entire map
+            SDL_FillRect(surface, nullptr, SDL_MapRGBA(surface->format, 255, 255, 255, 255));
+
+            for (auto y = 0; y < map.Height; y++)
+            {
+                for (auto x = 0; x < map.Width; x++)
+                {
+                    SDL_Surface *surface_asset = nullptr;
+
+                    auto &tile = map[Point(x, y)];
+
+                    if (!tile.IsOccupied())
+                    {
+                        if (tile.Asset == Asset::Type::EMPTY_SPACE)
+                        {
+                            surface_asset = BloodSword::Asset::Surface(Asset::Type::SELECT);
+                        }
+                        else if (tile.Asset != Asset::Type::NONE)
+                        {
+                            surface_asset = BloodSword::Asset::Surface(tile.Asset);
+                        }
+                        else if (tile.Type == Map::Object::EXIT)
+                        {
+                            surface_asset = BloodSword::Asset::Surface(Asset::Type::SELECT);
+                        }
+                    }
+
+                    if (surface_asset == nullptr)
+                    {
+                        // fill empty space
+                        surface_asset = Graphics::CreateSurface(BloodSword::TileSize, BloodSword::TileSize);
+
+                        SDL_FillRect(surface_asset, nullptr, SDL_MapRGBA(surface->format, 0, 0, 0, 255));
+                    }
+
+                    rect.x = x * BloodSword::TileSize + offset;
+
+                    rect.y = y * BloodSword::TileSize + offset;
+
+                    Graphics::RenderAssetThenFree(surface, surface_asset, rect);
+                }
+            }
+
+            // away players
+            BattlepitsRenderer::RenderPoints(surface, map.AwayPlayers, rect, offset, Asset::Type::CIRCLE, true);
+
+            // survivors
+            BattlepitsRenderer::RenderPoints(surface, map.Survivors, rect, offset, Asset::Type::SELECT, true);
+
+            // away opponents
+            BattlepitsRenderer::RenderPoints(surface, map.AwayOpponents, rect, offset, Asset::Type::SELECT, true);
+
+            // player locations
+            auto number = 1;
+
+            for (auto &origin : map.Origins)
+            {
+                rect.x = origin.X * BloodSword::TileSize + offset;
+
+                rect.y = origin.Y * BloodSword::TileSize + offset;
+
+                auto character_asset = BloodSword::Asset::Surface(Interface::Numbers[number]);
+
+                Graphics::RenderAssetThenFree(surface, character_asset, rect);
+
+                number++;
+            }
+
+            IMG_SavePNG(surface, image_file);
+
+            SDL_FreeSurface(surface);
+        }
+    }
+
+    // main loop
+    int Main(const char *module, const char *image_file)
+    {
+        auto return_code = 0;
+
+        try
+        {
+            BattlepitsRenderer::Render(module, image_file);
+        }
+        catch (std::exception &e)
+        {
+            std::cerr << std::endl
+                      << "BLOODSWORD ("
+                      << Version()
+                      << ") EXCEPTION: "
+                      << e.what()
+                      << std::endl
+                      << std::endl;
+
+            return_code = 1;
+        }
+
+        return return_code;
+    }
+}
+
+int main(int argc, char **argv)
+{
+    if (argc < 3)
+    {
+        std::cerr << "To run:" << std::endl
+                  << std::endl
+                  << argv[0] << "[module] [image.png]"
+                  << std::endl;
+
+        return 1;
+    }
+
+    return BloodSword::BattlepitsRenderer::Main(argv[1], argv[2]);
+}
---
+++ src/Room.hpp
@@ -0,0 +1,144 @@
+#ifndef __ROOM_HPP__
+#define __ROOM_HPP__
+
+#include "FieldOfView.hpp"
+#include "Random.hpp"
+
+namespace BloodSword::Room
+{
+    int MinWidth = 10;
+
+    int MaxWidth = 20;
+
+    class Base
+    {
+    public:
+        int Id = -1;
+
+        int X1 = -1;
+
+        int Y1 = -1;
+
+        int X2 = -1;
+
+        int Y2 = -1;
+
+        int Width = -1;
+
+        int Height = -1;
+
+        Point Center()
+        {
+            return Point(this->X1 + this->X2, this->Y1 + this->Y2) / 2;
+        }
+
+        std::pair<Point, Point> Inner()
+        {
+            return {Point(this->X1 + 1, this->Y1 + 1), Point(this->X2 -1, this->Y2 - 1)};
+        }
+
+        Base(int x, int y, int width, int height): X1(x), Y1(y), Width(width), Height(height)
+        {
+            this->X2 = this->X1 + this->Width;
+
+            this->Y2 = this->Y1 + this->Height;
+        }
+    };
+}
+
+namespace BloodSword::Battlepits
+{
+    Points TunnelBetween(Point start, Point end)
+    {
+        auto random = Random::Base();
+
+        Points points = {};
+
+        if (random.NextDouble() < 0.5)
+        {
+            for (auto x = start.X; x <= end.X; x++)
+            {
+                points.push_back(Point(x, start.Y));
+            }
+
+            for (auto y = start.Y; y <= end.Y; y++)
+            {
+                points.push_back(Point(end.X, y));
+            }
+        }
+        else
+        {
+            for (auto y = start.Y; y <= end.Y; y++)
+            {
+                points.push_back(Point(start.X, y));
+            }
+
+            for (auto x = start.X; x <= end.X; x++)
+            {
+                points.push_back(Point(x, end.Y));
+            }
+        }
+
+        return points;
+    }
+
+    void Place(Map::Base &map, Room::Base &room)
+    {
+        auto inner = room.Inner();
+
+        auto tl = inner.first;
+
+        auto br = inner.second;
+
+        for (auto y = room.Y1; y < room.Y2; y++)
+        {
+            for (auto x = room.X1; x < room.X2; x++)
+            {
+                map[Point(x, y)].Type = Map::Object::OBSTACLE;
+
+                map[Point(x, y)].Asset = Asset::Type::WALL;
+            }
+        }
+
+        for (auto y = tl.Y; y < br.Y; y++)
+        {
+            for (auto x = tl.X; x < br.X; x++)
+            {
+                map[Point(x, y)].Type = Map::Object::PASSABLE;
+
+                map[Point(x, y)].Asset = Asset::Type::NONE;
+            }
+        }
+    }
+
+    void Generate(Map::Base &map)
+    {
+        // clear
+        for (auto y = 0; y < map.Height; y++)
+        {
+            for (auto x = 0; x < map.Width; x++)
+            {
+                map[Point(x, y)].Type = Map::Object::NONE;
+
+                map[Point(x, y)].Asset = Asset::Type::IMPASSABLE;
+            }
+        }
+
+        auto room_1 = Room::Base(20, 15, 10, 15);
+
+        auto room_2 = Room::Base(35, 15, 10, 15);
+
+        Battlepits::Place(map, room_1);
+
+        Battlepits::Place(map, room_2);
+
+        for (auto point : TunnelBetween(room_1.Center(), room_2.Center()))
+        {
+            map[point].Type = Map::Object::PASSABLE;
+
+            map[point].Asset = Asset::Type::NONE;
+        }
+    }
+}
+
+#endif
